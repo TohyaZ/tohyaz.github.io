e97e57ff18b592ee7f6dde8e6f6e896f4f0f66fe
TohyaZ <heavencodd@gmail.com>
2023-05-19 19:08:49 +0700

    update-project-6

--- js/index.js
+++ js/index.js
@@ -1,2 +1,31 @@
-console.log("Hello")
+const getProductsCard = document.getElementById("sectionCard")
 
+const mockData = [
+    {
+    id: 1,
+    srcImg:"images/CloseUpP1020100_1500x.webp"
+    },
+    {
+    id: 2,
+    srcImg:"images/RF18XSpaceDustRendered_1800x1800_25201802-82e3-40c4-aa90-2080902b195b_1500x.webp"
+    },
+    {
+    id: 3,
+    srcImg:"images/Keyboards_1500x.webp"
+    },
+]
+
+let html = ""
+
+for (let index = 0 ; index < mockData.length ; index++)
+{
+    const card = `
+    <div class="col-lg-4 col-md-6 col-sm-12">
+        <div class="card shadow-sm mb-5 mt-5">
+            <img src=${mockData[index].srcImg}>
+        </div>
+    </div>
+    `
+    html += card
+}
+getProductsCard.innerHTML = html
