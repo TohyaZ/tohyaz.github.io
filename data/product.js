9343c70733225ddb485a6dce65bc3abb233acd36
TohyaZ <heavencodd@gmail.com>
2023-05-23 16:40:37 +0700

    update-project-7

---
+++ data/product.js
@@ -0,0 +1,38 @@
+const data2 = [
+    {
+    id: 7,
+    srcImg:"images/CloseUpP1020100_1500x.webp"
+    },
+    {
+    id: 8,
+    srcImg:"images/RF18XSpaceDustRendered_1800x1800_25201802-82e3-40c4-aa90-2080902b195b_1500x.webp"
+    },
+    {
+    id: 9,
+    srcImg:"images/Keyboards_1500x.webp"
+    },
+]
+
+const data = [
+    {
+        id:1,
+        srcImg:"images/Join65side.2_700x.webp",
+        title:"Product 1",
+        price:100
+    },
+     {
+        id:2,
+        srcImg:"images/TPS40Render_13_1903x_394c683f-f3a4-4dde-a3a5-626f96bc80cd_700x.webp",
+        title:"Product 2",
+        price:100
+    },
+     {
+        id:3,
+        srcImg:"images/20221231_700x.webp",
+        title:"Product 3",
+        price:100
+    },
+]
+
+export {data} 
+export {data2}
--- js/index.js
+++ js/index.js
@@ -1,31 +1,42 @@
+import { data } from "../data/product.js"
+import { data2 } from "../data/product.js"
 const getProductsCard = document.getElementById("sectionCard")
+const getContainerCard = document.getElementById("indexCcard")
 
-const mockData = [
-    {
-    id: 1,
-    srcImg:"images/CloseUpP1020100_1500x.webp"
-    },
-    {
-    id: 2,
-    srcImg:"images/RF18XSpaceDustRendered_1800x1800_25201802-82e3-40c4-aa90-2080902b195b_1500x.webp"
-    },
-    {
-    id: 3,
-    srcImg:"images/Keyboards_1500x.webp"
-    },
-]
+console.log("contai",getProductsCard)
+console.log("contai",getContainerCard)
 
-let html = ""
 
-for (let index = 0 ; index < mockData.length ; index++)
-{
-    const card = `
+let proDuct = ""
+data2.forEach((element) => {
+    let html = `
     <div class="col-lg-4 col-md-6 col-sm-12">
         <div class="card shadow-sm mb-5 mt-5">
-            <img src=${mockData[index].srcImg}>
+            <img src=${element.srcImg}>
         </div>
     </div>
+    `   
+    proDuct += html
+})
+
+let containerProduct = ""
+data.forEach((element) => {
+    let html = `
+    <div class="col-md-4">
+    <div class="card">
+        <img src=${element.srcImg}>
+        <div class="card-body">
+            <h5 class="card-title">1</h5>
+            <p class="card-text">2$</p>
+        </div>
+    </div>
+    </div>
     `
-    html += card
-}
-getProductsCard.innerHTML = html
+    containerProduct += html
+})
+
+
+getContainerCard.innerHTM = containerProduct;
+getProductsCard.innerHTML = proDuct;
+
+
